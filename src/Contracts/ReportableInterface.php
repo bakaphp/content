887ce3c92dde9c7f485f19d60b7c44fba9b9f668
arfenis <arfen00@gmail.com>
2021-12-13 14:06:30 -0400

    Rename reportable and add new method on interface

--- src/Contracts/ReportableInterface.php
+++ src/Contracts/ReportableInterface.php
@@ -6,9 +6,10 @@ namespace Kanvas\Content\Contracts;
 
 use Baka\Contracts\Database\ModelInterface;
 
-interface ReportInterface
+interface ReportableInterface
 {
     public function isOpen() : bool;
     public function isClosed() : bool;
     public function getEntityData() : ?ModelInterface;
+    public function getId() : int;
 }
