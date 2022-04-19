5f6b435c236550ea7715f0f3c568ce28599f7ff0
kaioken <max@mctekk.com>
2022-04-19 01:52:54 -0400

    feat : enums

---
+++ src/Enums/Status.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanvas\Content\Enums;
+
+class Status
+{
+    const PUBLISHED = 1;
+    const DRAFT = 0;
+    const SCHEDULED = 2;
+    const PUBLIC = 1;
+    const PRIVATE = 0;
+    const DELETED = 0;
+}
